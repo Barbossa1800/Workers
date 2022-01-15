7ba39dccaf00db4be2d306e345f4d477e265b94e
Nikita Medko <nikita.jove28@gmail.com>
2022-01-15 17:07:24 +0200

    New controller was created StatusTaskController and create view Create, GetAll. Edited view _Layout (editing of  tag helper);

---
+++ Workers/Workers.Web/Controllers/StatusTaskController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Workers.Web.Infrastructure.Context;
+using Workers.Web.Infrastructure.Models;
+
+namespace Workers.Web.Controllers
+{
+    [Route("status-task")]
+    public class StatusTaskController : Controller
+    {
+        private readonly WorkerDbContext _db;
+        public StatusTaskController(WorkerDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var statusTask = await _db.StatusTasks.ToListAsync();
+            return View(statusTask);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(StatusTask statusTask)
+        {
+            await _db.AddAsync(statusTask);
+            await _db.SaveChangesAsync();
+            return LocalRedirect("~/status-task/all");
+        }
+    }
+}
