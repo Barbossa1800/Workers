fd912571bf3e4714e8274e59de147b174a044afe
Nikita Medko <nikita.jove28@gmail.com>
2022-01-15 16:46:59 +0200

    Add new controller TaskController. Create view for TaskController. Edit View _Layout.cshtml

---
+++ Workers/Workers.Web/Controllers/TaskController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Workers.Web.Infrastructure.Context;
+using Workers.Web.Infrastructure.Models;
+
+namespace Workers.Web.Controllers
+{
+    [Route("task")]
+    public class TaskController : Controller
+    {
+        private readonly WorkerDbContext _db;
+
+        public TaskController(WorkerDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("all")]
+        public  async Task<IActionResult> GetAll()
+        {
+            var tasks = await _db.Tasks.Include(x => x.Project).Include(x =>x.InfoStatus).ToListAsync();
+            return View(tasks);
+        }
+
+        [HttpGet("create")]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Projects = await _db.Projects.ToListAsync();
+            ViewBag.StatusTasks = await _db.StatusTasks.ToListAsync();
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(Infrastructure.Models.Task task)
+        {
+            await _db.Tasks.AddAsync(task);
+            await _db.SaveChangesAsync();
+            return LocalRedirect("~/task/all");
+        }
+    }
+}
